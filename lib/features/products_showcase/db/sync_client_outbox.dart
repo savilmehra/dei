31fd6b74f625edc9e3ae67a6d90f8045f10dcddf
Savil Kumar <savil.kumar@borngroup.com>
2022-10-31 19:35:58 +0530

    qr code scanner

--- lib/features/products_showcase/db/sync_client_outbox.dart
+++ lib/features/products_showcase/db/sync_client_outbox.dart
@@ -37,16 +37,67 @@ sudo ss -tulpn | grep LISTEN
 sudo lsof -i:22 ## see a specific port such as 22 ##
 sudo nmap -sTU -O IP-address-Here*/
 /*
+WARNING: The requested image's platform (linux/amd64) does not match the detected host platform (linux/arm64/v8) and no specific platform was requested
+001-15:33:54.9943 [INFO ] [SySvAp] Starting ObjectBox Sync Server version 2 (protocol version: 3, core: 2.9.2-2021-05-13 (Sync))
+001-15:33:55.0022 [INFO ] [SySvAp] Default configuration file sync-server-config.js not found
+001-15:33:55.0045 [INFO ] [SvrMsg] Registering server transport for ws
+001-15:33:55.5290 [INFO ] [SySvAp] Model file ./data/objectbox-model.json loaded, it has 5184 bytes
+001-15:33:55.5420 [INFO ] [SySvAp] Given model was found by hash as an existing schema version ID 1
+001-15:33:55.5422 [INFO ] [SySvAp] Starting the server with schema version ID 1
+001-15:33:55.5649 [INFO ] [SvrUws] UwsServer listening on all interfaces, port 9999 without SSL
+001-15:33:55.5658 [WARN ] [SvSync] UNSECURED no-authentication mode enabled - every client allowed without any validation
+001-15:33:55.5713 [INFO ] [SvSync] Started on port 9999
+001-15:33:55.5714 [INFO ] [SySvAp] Starting object browser on 0.0.0.0:9980
+001-15:33:55.5733 [INFO ] [HttpSv] Running in single-store mode with an already opened store
+001-15:33:55.5736 [INFO ] [HttpSv] Listening on 0.0.0.0:9980
+001-15:33:55.5737 [WARN ] [HttpSv] UNSECURED no-authentication mode enabled:
+!!! Allowing access to anyone without any user validation
+!!! After e.g. adjusting user logins, please restart asap to secure the server again
+001-15:33:55.5737 [INFO ] [HttpSv] User management: enabled
+001-15:33:55.5809 [INFO ] [HttpSv] HttpServer listening on all interfaces, port 9980
+001-15:33:55.5829 [INFO ] [SySvAp] ObjectBox sync server started in 589 ms
+
+
+
+
+
+WARNING: The requested image's platform (linux/amd64) does not match the detected host platform (linux/arm64/v8) and no specific platform was requested
+001-15:40:33.8628 [INFO ] [SvSyAp] Starting ObjectBox Sync Server version 3 (protocol version: 4, core: 3.3.1-2022-10-14 (SyncServer, admin, tree, dlog))
+001-15:40:33.8721 [INFO ] [SvSyAp] Loading configuration file ./data/sync-server-config.json
+001-15:40:33.8785 [INFO ] [SvrMsg] Registering server transport for ws
+001-15:40:34.3985 [INFO ] [SvSyAp] Model file ./data/objectbox-model.json loaded, it has 5184 bytes
+001-15:40:34.4096 [INFO ] [SvSyAp] Given model was found by hash as an existing schema version ID 1
+001-15:40:34.4098 [INFO ] [SvSyAp] Starting the server with schema version ID 1
+001-15:40:34.4286 [INFO ] [SvWbSo] Initializing for all interfaces, port 9999 without SSL
+001-15:40:34.4307 [WARN ] [SvSync] <9C3A> UNSECURED no-authentication mode enabled - every client allowed without any validation
+001-15:40:34.4357 [INFO ] [SvClst] <9C3A> Starting 9C3A923C as part of cluster "cluster-born" (fixed follower); sequence committed: 0-0-00000000, draft: same
+001-15:40:34.4374 [INFO ] [SvClst] <9C3A> Starting 1 peer(s) connections...
+001-15:40:34.4455 [INFO ] [ClTxAQ] <9C3A> Incoming TXs will be announced (non-quiet mode)
+001-15:40:34.4495 [INFO ] [SvClCl] Peer #1 started (state sequence: 0/0/1)
+001-15:40:34.4535 [INFO ] [SvSync] <9C3A> Started on port 9999 (peer ID: 9C3A923C)
+001-15:40:34.4550 [INFO ] [SvSync] <9C3A> Cluster ID: cluster-born
+001-15:40:34.4553 [INFO ] [SvSyAp] Starting admin server on http://0.0.0.0:9980
+001-15:40:34.4615 [INFO ] [SvHttp] Running in single-store mode with an already opened store
+001-15:40:34.4616 [INFO ] [SvHttp] Listening on http://0.0.0.0:9980
+001-15:40:34.4617 [WARN ] [SvHttp] UNSECURED no-authentication mode enabled:
+!!! Allowing access to anyone without any user validation
+!!! After e.g. adjusting user logins, please restart asap to secure the server again
+001-15:40:34.4617 [INFO ] [SvHttp] User management: enabled
+001-15:40:34.4688 [INFO ] [SvHttp] HttpServer listening on all interfaces, port 9980
+001-15:40:34.4717 [INFO ] [SvSyAp] ObjectBox sync server started in 609 ms
+*/
+
+/*
 docker run --rm -it \
     --volume $(pwd):/data   \
-      --publish 127.0.0.1:9991:9991  \
-   --publish 127.0.0.1:9981:9981   \
+      --publish 127.0.0.1:9999:9999  \
+   --publish 127.0.0.1:9980:9980   \
      --user $UID\
-
-       objectboxio/sync:sync-server-2022-10-21 \
+ objectboxio/sync:sync-server-2022-10-21 \
        --model ./data/objectbox-model.json \
          --conf ./data/sync-server-config.json \
          --unsecured-no-authentication \
+
 --debug
 
 
@@ -65,7 +116,7 @@ docker run --rm -it \
 // ssh -i /Users/born/Desktop/privatekey.pem  savil@35.154.207.75
 //sudo netstat -tupln
 
- //   ./sync-server --model=objectbox-model.json --unsecured-no-authentication --debug
+ //   ./sync-server --model=objectbox-model.json  --conf sync-server-config.json --unsecured-no-authentication --debug
 //https://www.guru99.com/the-vi-editor.html
 class ObjectBoxSyncClient {
   Store? _store;
@@ -102,11 +153,10 @@ print("open-------------------------new ");
         _store = store;
         Sync.client(
           store,
-          'ws://35.154.207.75:9999',
+          'ws://$syncServerIp:9999',
           SyncCredentials.none(),
         ).start();
         productBox = store.box<Items>();
-
         stream = productBox!
             .query()
             .watch(triggerImmediately: true)
